4be0fd73d218a3d5b09043b088cb476d1fa3bd2b
Hibiki Moreno <hibikicarlo@gmail.com>
2026-03-03 11:49:36 -0600

    proyecto inicializado y configurado para flowbite

---
+++ resources/views/loginScreen/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi App con Flowbite</title>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @yield('content')
+        
+
+<div class="p-4 mb-4 text-sm text-fg-brand-strong rounded-base bg-brand-softer" role="alert">
+  <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+</div>
+<div class="p-4 mb-4 text-sm text-fg-danger-strong rounded-base bg-danger-soft" role="alert">
+  <span class="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+</div>
+<div class="p-4 mb-4 text-sm text-fg-success-strong rounded-base bg-success-soft" role="alert">
+  <span class="font-medium">Success alert!</span> Change a few things up and try submitting again.
+</div>
+<div class="p-4 mb-4 text-sm text-fg-warning rounded-base bg-warning-soft" role="alert">
+  <span class="font-medium">Warning alert!</span> Change a few things up and try submitting again.
+</div>
+<div class="p-4 text-sm text-heading rounded-base bg-neutral-secondary-medium" role="alert">
+  <span class="font-medium">Dark alert!</span> Change a few things up and try submitting again.
+</div>
+
+</html>
