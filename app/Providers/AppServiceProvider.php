ab47d25c8e4dd8b955d807a8fca7d3277bcb76ed
altamanquez <2123100467@soy.utj.edu.mx>
2026-03-13 19:40:08 -0600

    olas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-        if (app()->environment("production")) { URL::forceScheme("https"); }
     {
-        //
+        if (app()->environment("production")) { 
+            URL::forceScheme("https"); 
+        }
     }
 }
