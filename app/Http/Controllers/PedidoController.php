8d8c179c1bc1b2ff5d9f93c88bf7d82a6decee3c
AlejandroHZ8A <2124100169@soy.utj.edu.mx>
2026-04-11 21:43:27 -0600

    cambios de pedidos a ver si funciona

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -11,7 +11,7 @@ class PedidoController extends Controller
 
     public function __construct()
     {
-        $this->apiUrl = 'http://127.0.0.1:8000/api';
+        $this->apiUrl = env('API_URL', '127.0.0.1:8000/api');
     }
 
     public function index()
